fb60015b77d0afadbe754877dc6682a159150181
KushanTharaka97 <tharakakushan@gmail.com>
2021-04-05 03:31:14 +0530

    Making the data model and the logics.

--- src/main/java/com/frankmoley/lil/learningspring/data/repository/RoomRepository.java
+++ src/main/java/com/frankmoley/lil/learningspring/data/repository/RoomRepository.java
@@ -1,8 +1,11 @@
 package com.frankmoley.lil.learningspring.data.repository;
 
+import com.frankmoley.lil.learningspring.data.entity.Reservation;
 import com.frankmoley.lil.learningspring.data.entity.Room;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
 
 @Repository
 public interface RoomRepository extends CrudRepository<Room, Long> {
